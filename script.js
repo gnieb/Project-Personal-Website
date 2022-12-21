53f675b83f3c42804431e9d800d5de19c1dd1a0e
Grace Nieboer <grace.nieboer@outlook.com>
2022-12-20 21:22:03 -0500

    final additions

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by javascript";
+h2.textContent = "Stay hydrated, friends.";
 document.querySelector("body").appendChild(h2);
+
+
+function eventListenerClick () {
+    const text = document.getElementById("bye");
+
+    function clickMessage () {
+        alert("Come again soon!");
+    }
+
+    text.addEventListener('click', clickMessage );
+}
+
+eventListenerClick();
